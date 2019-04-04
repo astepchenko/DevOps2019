423326e0b78ed98abbcc8cf6f793b62b0d6c637b
Aleksandr Stepchenko <aleksandr_stepchenko@epam.com>
2019-04-04 12:43:14 +0300

    fixed recipe

--- blue_green/recipes/default.rb
+++ blue_green/recipes/default.rb
@@ -13,9 +13,14 @@ end
 
 bash 'blue_vs_green' do
   code <<-EOH
-    curl -s http://localhost:8080/greeter && \
-    (docker run -d -p 8081:8080 --name greeter-green "mate:5000/greeter:#{node['ver']}" && docker rm greeter-blue --force || true) || \
-    (docker run -d -p 8080:8080 --name greeter-blue "mate:5000/greeter:#{node['ver']}" && docker rm greeter-green --force || true)
+    if ! (true &>/dev/null </dev/tcp/localhost/8080);
+    then
+      docker run -d -p 8080:8080 --name greeter-blue "mate:5000/greeter:#{node['ver']}" && sleep 5
+      (true &>/dev/null </dev/tcp/localhost/8080) && (echo 'blue ok' && docker rm greeter-green --force > /dev/null || true) || echo 'blue fail'
+    else
+      docker run -d -p 8081:8080 --name greeter-green "mate:5000/greeter:#{node['ver']}" && sleep 5
+      (true &>/dev/null </dev/tcp/localhost/8081) && (echo 'green ok' && docker rm greeter-blue --force > /dev/null || true) || echo 'green fail'
+    fi
   EOH
   action :run
 end
