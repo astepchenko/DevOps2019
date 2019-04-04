43f5d8ae4b5ef288214acce1615749ed01167e77
Aleksandr Stepchenko <alexander.stepchenko@gmail.com>
2019-04-04 11:26:50 +0000

    version: 1.0.22, environment: deploy

--- blue_green/metadata.rb
+++ blue_green/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures blue_green'
 long_description 'Installs/Configures blue_green'
-version '0.1.23'
+version '0.1.24'
 chef_version '>= 13.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
