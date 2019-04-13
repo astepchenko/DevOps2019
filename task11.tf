2d277874176d71d11ded68c9ac4f5097665cdd97
Aleksandr Stepchenko <aleksandr_stepchenko@epam.com>
2019-04-13 12:58:14 +0300

    fixed 8080 leak on nodes

--- task11.tf
+++ task11.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "nodes" {
     from_port = 8080
     to_port = 8080
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = ["${aws_security_group.elb.id}"]
   }
   ingress {
     from_port = 22
